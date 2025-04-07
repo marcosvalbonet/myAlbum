56ec465ad0c3d09d1d49f629d28a458967331e0d
marcosvalbonet <marcos.valbonet@gmail.com>
2025-04-07 13:35:00 +0200

    feat: Add Hilt and main activity

---
+++ app/src/main/java/com/marcosval/myalbum/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.marcosval.myalbum
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.marcosval.myalbum.ui.screen.ItemScreen
+import com.marcosval.myalbum.ui.theme.MyAlbumTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyAlbumTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ItemScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyAlbumTheme {
+        Greeting("Android")
+    }
+}
